d42d4a49c95e191cdf7526ba4b24f09b455d8ae5
MichelLimaDesigner <41237418+MichelLimaDesigner@users.noreply.github.com>
2018-07-30 22:47:51 -0300

    Eye Animation
    A simple animation of an eye using the location of the mouse to start the animation of the eye position

---
+++ main.js
@@ -0,0 +1,136 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+                DIV TOPO DIREITO
+
+------------------------ DIV topDireita4 */
+$(document).ready(function(){
+    $("#topDireita4").mouseover(function(){
+        $(".iris").css("transform", "translate(40px, 5px)");
+        $(".preto").css("transform", "translate(20px, 0px)");
+    })
+});
+/*------------------------ DIV topDireita3 */
+$(document).ready(function(){
+    $("#topDireita3").mouseover(function(){
+        $(".iris").css("transform", "translate(40px, -35px)");
+        $(".preto").css("transform", "translate(20px, -10px)");
+    })
+});
+
+/*------------------------ DIV topDireita2 */
+$(document).ready(function(){
+    $("#topDireita2").mouseover(function(){
+        $(".iris").css("transform", "translate(40px, -55px)");
+        $(".preto").css("transform", "translate(20px, -20px)");
+    })
+});
+
+/*------------------------ DIV topDireita1 */
+$(document).ready(function(){
+    $("#topDireita1").mouseover(function(){
+        $(".iris").css("transform", "translate(35px, -55px)");
+        $(".preto").css("transform", "translate(20px, -30px)");
+    })
+});
+
+/*-------------------------------------------------------------------------------------------------------------------------
+                DIV TOPO ESQUERDO
+
+------------------------ DIV topEsquerda4 */
+$(document).ready(function(){
+    $("#topEsquerda4").mouseover(function(){
+        $(".iris").css("transform", "translate(-40px, -5px)");
+        $(".preto").css("transform", "translate(-20px, 0px)");
+    })
+});
+/*------------------------ DIV topEsquerda3 */
+$(document).ready(function(){
+    $("#topEsquerda3").mouseover(function(){
+        $(".iris").css("transform", "translate(-40px, -35px)");
+        $(".preto").css("transform", "translate(-20px, -10px)");
+    })
+});
+
+/*------------------------ DIV topEsquerda2 */
+$(document).ready(function(){
+    $("#topEsquerda2").mouseover(function(){
+        $(".iris").css("transform", "translate(-40px, -55px)");
+        $(".preto").css("transform", "translate(-20px, -20px)");
+    })
+});
+
+/*------------------------ DIV topEsquerda1 */
+$(document).ready(function(){
+    $("#topEsquerda1").mouseover(function(){
+        $(".iris").css("transform", "translate(-35px, -55px)");
+        $(".preto").css("transform", "translate(-20px, -30px)");
+    })
+});
+
+/*-------------------------------------------------------------------------------------------------------------------------
+                DIV BOTTOM DIREITA
+
+------------------------ DIV bottomDireita1 */
+$(document).ready(function(){
+    $("#bottomDireita1").mouseover(function(){
+        $(".iris").css("transform", "translate(40px, 25px)");
+        $(".preto").css("transform", "translate(20px, 10px)");
+    })
+});
+/*------------------------ DIV bottomDireita2 */
+$(document).ready(function(){
+    $("#bottomDireita2").mouseover(function(){
+        $(".iris").css("transform", "translate(40px, 35px)");
+        $(".preto").css("transform", "translate(20px, 20px)");
+    })
+});
+
+/*------------------------ DIV bottomDireita3 */
+$(document).ready(function(){
+    $("#bottomDireita3").mouseover(function(){
+        $(".iris").css("transform", "translate(40px, 45px)");
+        $(".preto").css("transform", "translate(20px, 22px)");
+    })
+});
+
+/*------------------------ DIV bottomDireita4 */
+$(document).ready(function(){
+    $("#bottomDireita4").mouseover(function(){
+        $(".iris").css("transform", "translate(35px, 55px)");
+        $(".preto").css("transform", "translate(20px, 30px)");
+    })
+});
+
+/*-------------------------------------------------------------------------------------------------------------------------
+                DIV BOTTOM ESQUERDA
+
+------------------------ DIV bottomEsquerda1 */
+$(document).ready(function(){
+    $("#bottomEsquerda1").mouseover(function(){
+        $(".iris").css("transform", "translate(-40px, 25px)");
+        $(".preto").css("transform", "translate(-20px, 10px)");
+    })
+});
+/*------------------------ DIV bottomEsquerda2 */
+$(document).ready(function(){
+    $("#bottomEsquerda2").mouseover(function(){
+        $(".iris").css("transform", "translate(-40px, 35px)");
+        $(".preto").css("transform", "translate(-20px, 20px)");
+    })
+});
+
+/*------------------------ DIV bottomEsquerda3 */
+$(document).ready(function(){
+    $("#bottomEsquerda3").mouseover(function(){
+        $(".iris").css("transform", "translate(-40px, 45px)");
+        $(".preto").css("transform", "translate(-20px, 22px)");
+    })
+});
+
+/*------------------------ DIV bottomEsquerda4 */
+$(document).ready(function(){
+    $("#bottomEsquerda4").mouseover(function(){
+        $(".iris").css("transform", "translate(-35px, 55px)");
+        $(".preto").css("transform", "translate(-20px, 30px)");
+    })
+});
+
